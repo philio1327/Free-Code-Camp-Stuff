ee7417804174689e1b792907f3429eb3d83711b1
philio1327 <philipanderegg@hotmail.com>
2025-06-19 19:11:44 -0400

    Built a Loan Qualification Checker in JS and formatted indentation

--- 03-JavaScript/14-build_an_email_masker.js
+++ 03-JavaScript/14-build_an_email_masker.js
@@ -1,12 +1,12 @@
-function maskEmail(email){
-  let index = email.indexOf("@");
-  if (index === -1){
-    return "Invalid Email";
-  }
-  let numStars = index - 2;
-  let stars = "*".repeat(numStars);
-  let returnString = `${email[0]}${stars}${email[index-1]}${email.slice(index)}`;
-  return returnString;
+function maskEmail(email) {
+    let index = email.indexOf("@");
+    if (index === -1) {
+        return "Invalid Email";
+    }
+    let numStars = index - 2;
+    let stars = "*".repeat(numStars);
+    let returnString = `${email[0]}${stars}${email[index - 1]}${email.slice(index)}`;
+    return returnString;
 }
 
 let email = "apple.pie@example.com";
