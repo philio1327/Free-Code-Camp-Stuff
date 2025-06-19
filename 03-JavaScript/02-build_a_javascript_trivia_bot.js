e4c6ba6a63cc0f132328dce1de41e117d83f7e85
philio1327 <philipanderegg@hotmail.com>
2025-06-19 14:54:53 -0400

    Created JS files and integrated them into HTML file
    Also created JavaScript Variables, Data Types Review File

---
+++ 03-JavaScript/02-build_a_javascript_trivia_bot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Jeffrey";
+botLocation = "Tokyo";
+let favoriteLanguage = "Python";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is an interpreted language.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " allows for Object Oriented Programming and has dynamic typing."
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is often used in LLM creation and AI modelling."
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ 03-JavaScript/03-build_a_sentence_maker.js
@@ -0,0 +1,23 @@
+let adjective = "orange";
+let noun = "cat";
+let verb = "triggered";
+let place = "shack";
+let adjective2 = "enormous";
+let noun2 = "politician";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "crooked";
+noun = "man";
+verb = "stroked";
+place = "mansion";
+adjective2 = "narrow";
+noun2 = "lobster";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
