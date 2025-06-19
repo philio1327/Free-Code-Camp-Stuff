1c81a8bf9aa335a783d2a7e3c7d6f78fa34456d5
philio1327 <philipanderegg@hotmail.com>
2025-06-19 19:33:17 -0400

    Created Implement Truncate String algorithm

---
+++ 03-JavaScript/17-implement_truncate_string.js
@@ -0,0 +1,14 @@
+function truncateString(string, length) {
+    if (length >= string.length || length < 0) { //account for negative lengths
+        return string;
+    }
+    else {
+        return `${string.slice(0, length)}...`;
+    }
+}
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2));
+console.log(truncateString("A-", 1));
+console.log(truncateString("Absolutely Longer", 2));
