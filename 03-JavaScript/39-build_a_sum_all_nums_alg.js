fbb9381585480fb4fea763eded411ef2e7c6a4e7
philio1327 <philipanderegg@hotmail.com>
2025-06-21 19:36:35 -0400

    Created JS Fundamentals Doc, sum all nums JS file, and HTML to run mutliple JS files

---
+++ 03-JavaScript/39-build_a_sum_all_nums_alg.js
@@ -0,0 +1,21 @@
+function sumAll(arr1) {
+    let num1 = arr1[0];
+    let num2 = arr1[1];
+    let sum = 0;
+    let lower, higher;
+    if (num1 < num2) {
+        lower = num1;
+        higher = num2;
+    } else {
+        lower = num2;
+        higher = num1;
+    }
+    for (let i = lower; i <= higher; i++) {
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumAll([1, 4])) //10
+console.log(sumAll([4, 1])) //10
+console.log(sumAll([5, 10])) //45
+console.log(sumAll([10, 5])) //45
