123f36f845921a9213325dab3d22c0a4abf0a358
philio1327 <philipanderegg@hotmail.com>
2025-06-21 11:05:35 -0400

    Built a Factorial Calculator in JS

---
+++ 03-JavaScript/28-build_a_factorial_calculator.js
@@ -0,0 +1,16 @@
+// Better to do this in a different language such as Python to take advantage of value caching, C++ for performance or CPython for a hybrid
+
+let num = 15;
+
+function factorialCalculator(arg){
+  let result = 1;
+  for (let i=1; i<=arg; i++){
+    result *= i;
+  }
+  return result;
+}
+console.log(factorialCalculator(5));
+console.log(factorialCalculator(7));
+let factorial = factorialCalculator(num);
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
