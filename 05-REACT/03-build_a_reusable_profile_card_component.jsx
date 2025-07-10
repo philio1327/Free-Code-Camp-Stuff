46d29e1fdd5e2947fe29404215f6fdee446352e5
philio1327 <philipanderegg@hotmail.com>
2025-07-10 19:29:34 -0400

    Created a reusable profile Card in REACT

---
+++ 05-REACT/03-build_a_reusable_profile_card_component.jsx
@@ -0,0 +1,31 @@
+export function Card({ name, title, bio }) {
+    return (
+        <div className="card">
+            <h2>{name}</h2>
+            <p className="card-title">{title}</p>
+            <p>{bio}</p>
+        </div>
+    )
+}
+
+export function App() {
+    return (
+        <div className="flex-container">
+            <Card
+                name="Mark"
+                title="Frontend developer"
+                bio="I like to work with different frontend technologies and play video games."
+            />
+            <Card
+                name="Tiffany"
+                title="Engineering manager"
+                bio="I have worked in tech for 15 years and love to help people grow in this industry."
+            />
+            <Card
+                name="Doug"
+                title="Backend developer"
+                bio="I have been a software developer for over 20 years and I love working with Go and Rust."
+            />
+        </div>
+    );
+}
