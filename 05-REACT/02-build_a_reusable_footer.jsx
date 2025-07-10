98650ca6af96926227c1f607e67ff0d6a5e15874
philio1327 <philipanderegg@hotmail.com>
2025-07-09 21:57:09 -0400

    Build a reusable Footer

---
+++ 05-REACT/02-build_a_reusable_footer.jsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li><a href="#">Guide to Healthy Eating</a></li>
+                <li><a href="#">Workout Guide</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Fitness on the Go</a></li>
+                <li><a href="#">Wellness Centre</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+            </ul>
+            <p>© Phil 2025</p>
+        </footer>
+    );
+};
